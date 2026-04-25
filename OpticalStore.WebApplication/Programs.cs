acde595995abde6219f273c8d8eccb60b3e56091
TankBoy143 <phucdo14032005@gmail.com>
2026-04-25 23:40:46 +0700

    Add session management to the application

---
+++ OpticalStore.WebApplication/Programs.cs
@@ -0,0 +1,16 @@
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.UseSession();  
+
+app.UseAuthorization();
